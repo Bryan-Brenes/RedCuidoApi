469f84faabc35213bd2c7450a49ba77cbfae6b57
Bryan Brenes <bryanbrenes1@gmail.com>
2019-11-09 00:51:40 -0600

    permitir cors

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ pool.on('error', err => {
 // con esto se puede usar el req.body
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/pruebaBase', (req, res) => {
     poolConnect.then(() => {
         console.log('Conectado');
